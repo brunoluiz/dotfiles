44c7adc8ee0188390288a388f514f3df79e4a5f9
Bruno Luiz Silva <contact@brunoluiz.net>
2024-08-14 17:41:05 +0100

    chore: update init shell scripts

--- macos.sh
+++ macos.sh
@@ -9,26 +9,22 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 # Installs tpm plugin system
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-# Installs neovim with python support
-python3 -m pip install --user --upgrade pynvim
-
 # Installs all dependencies from Brewfile
 brew bundle install --file=.Brewfile
 
 # Installs krew -- kubectl plugin manager
 (
-  set -x; cd "$(mktemp -d)" &&
-  OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
-  ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
-  KREW="krew-${OS}_${ARCH}" &&
-  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&
-  tar zxvf "${KREW}.tar.gz" &&
-  ./"${KREW}" install krew
+  set -x
+  cd "$(mktemp -d)" &&
+    OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
+    ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
+    KREW="krew-${OS}_${ARCH}" &&
+    curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&
+    tar zxvf "${KREW}.tar.gz" &&
+    ./"${KREW}" install krew
 )
 
-# asdf plugins
-asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
-asdf plugin-add terraform https://github.com/asdf-community/asdf-hashicorp.git
-asdf plugin add golang https://github.com/asdf-community/asdf-golang.git
-asdf plugin-add kubectl https://github.com/asdf-community/asdf-kubectl.git
-
+mise use --global node@22
+mise use --global golang@1.23
+mise use --global kubectl@1.30
+mise use --global terraform@1.5
