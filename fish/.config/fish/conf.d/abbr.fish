809efaf02a01d91d4d876e1770a7c2336dc7b3fb
Bruno Luiz Silva <contact@brunoluiz.net>
2025-08-01 11:57:22 +0100

    move abbr to aliases in some cases

--- fish/.config/fish/conf.d/abbr.fish
+++ fish/.config/fish/conf.d/abbr.fish
@@ -1,19 +1,8 @@
-abbr k kubectl
-abbr kns kubens
-abbr kcx kubectx
 abbr q 'q -d ,'
-abbr vim /opt/homebrew/bin/nvim
-abbr vi /opt/homebrew/bin/nvim
-abbr tmux tmux
-abbr tf terraform
 abbr gag git again
 
 # Override usual tools
-abbr ls lsd
 abbr l 'ls -l'
 abbr la 'ls -a'
 abbr lla 'ls -la'
 abbr lt 'ls --tree'
-abbr cat bat
-abbr less bat
-abbr ps procs
---
+++ fish/.config/fish/conf.d/alias.fish
@@ -0,0 +1,12 @@
+alias k=kubectl
+alias kns=kubens
+alias kcx=kubectx
+alias vim=/opt/homebrew/bin/nvim
+alias vi=/opt/homebrew/bin/nvim
+alias tf=terraform
+
+# Override usual tools
+alias ls=lsd
+alias cat=bat
+alias less=bat
+alias ps=procs
