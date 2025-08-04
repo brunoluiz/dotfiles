b0e4c80862eb4eed1b26b2a3fc63657ab5101ba5
Bruno Luiz Silva <contact@brunoluiz.net>
2025-08-04 18:47:19 +0100

    chore: wezterm nicer UI

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -10,7 +10,7 @@ config.font_size = 12.0
 
 -- Window management
 config.native_macos_fullscreen_mode = true
--- config.window_decorations = "RESIZE"
+config.window_decorations = "RESIZE"
 config.hide_tab_bar_if_only_one_tab = true
 config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
 
