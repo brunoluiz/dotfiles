9bd764375471b77f629eae0c67e952958dd56b9e
Bruno Luiz Silva <contact@brunoluiz.net>
2025-08-04 19:32:38 +0100

    chore: tweak wezterm

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -10,8 +10,8 @@ config.font_size = 12.0
 
 -- Window management
 config.native_macos_fullscreen_mode = true
-config.window_decorations = "RESIZE"
-config.hide_tab_bar_if_only_one_tab = true
+config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
+config.hide_tab_bar_if_only_one_tab = false
 config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
 
 -- Mouse bindings
