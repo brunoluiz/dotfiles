3640246bf73e564bf7f948649c5d8901fe1a1fa0
Bruno Luiz Silva <contact@brunoluiz.net>
2026-01-22 21:36:33 +0000

    chore(nvim): deal with hidden files for explorer/search

--- nvim/.config/nvim/lua/plugins/overrides.lua
+++ nvim/.config/nvim/lua/plugins/overrides.lua
@@ -1,3 +1,22 @@
 return {
   { import = "plugins.overrides_hcl" },
+  {
+    "folke/snacks.nvim",
+    opts = {
+      explorer = {
+        hidden = true,
+        exclude = { ".git" },
+      },
+      picker = {
+        hidden = true,
+        sources = {
+          files = {
+            hidden = true,
+            ignored = true,
+          },
+        },
+        file_ignore_patterns = { ".git/" },
+      },
+    },
+  },
 }
