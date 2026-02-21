cdf2f6c3f0f8250992dbffdabd91e8e6b829aca2
Bruno Luiz Silva <contact@brunoluiz.net>
2026-02-21 23:31:49 +0000

    feat: add github browse

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set({ "n", "x" }, "<leader>gO", function()
+  Snacks.gitbrowse()
+end, { desc = "Git Browse (open)" })
